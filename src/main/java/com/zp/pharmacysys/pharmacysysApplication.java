3cbb12c792ca7647a30d4056d896c8422f32b51b
xukun3 <xukun3@asiainfo.com>
2019-03-01 15:12:52 +0800

    初始化

---
+++ src/main/java/com/zp/pharmacysys/pharmacysysApplication.java
@@ -0,0 +1,11 @@
+package com.zp.pharmacysys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class pharmacysysApplication {
+	public static void main(String[] args) {
+        SpringApplication.run(pharmacysysApplication.class, args);
+    }
+}
